3a7a5a9fef6181c9dff7909c66a3ccf1dd6ccd9b
nazavod <91217742+nazavod777@users.noreply.github.com>
2023-12-19 16:17:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ DATA_COUNT: int = 1  # Количество выдачи данных одном
 PROXY_URL: str | None = ''  # PROXY (может быть пустым). Формат - type://user:pass@ip:port / type://user:pass:ip:port
 # / type://ip:port:user:pass / type://ip:port
 APPEND_TEXT: str | None = '\n\nThis Text'  # Текст, который можно добавлять к каждому своему ответу (можно оставить пустым)
+MESSAGE_CONTAINS: str | None = ''  # Будет выдавать только пользователям, чье сообщение содержит этот текст
+MESSAGE_EQUAL: str | None = ''  # Будет выдавать только пользователям, чье сообщение равно этому
